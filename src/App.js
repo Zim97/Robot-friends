f79a9c2df55f77da9dfcb45629ffff91349789a0
Zim97 <112665921+Zim97@users.noreply.github.com>
2023-12-13 22:56:49 -0500

    filtering through the robots

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import SearchBox from "./searchBox";
+import CardList from "./cardList";
+import { robots } from "./robots";
+
+class App extends Component{
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfield: "",
+    };
+  }
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value });
+  };
+  render() {
+    const filterRobots = this.state.robots.filter((robot) => {
+      return robot.name
+        .toLowerCase()
+        .includes(this.state.searchfield.toLowerCase());
+    });
+    return (
+      <div className="tc">
+        <h1>Robo Friends</h1>
+        <SearchBox searchChange={this.onSearchChange} />
+        <CardList robots={filterRobots} />;
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/card.js
+++ src/card.js
@@ -3,7 +3,7 @@ import React from "react";
 const Card = (props) => {
   return (
     <div className=" tc bg-light-blue dib br3 pa3 ma2 grow bw2 gorw shadow-5">
-      <img src={`https://robohash.org/${props.id}?200x200`} alt='robots'></img>
+      <img src={`https://robohash.org/${props.id}?200x200`} alt="robots"></img>
       <div>
         <h2>{props.name}</h2>
         <p>{props.email}</p>
